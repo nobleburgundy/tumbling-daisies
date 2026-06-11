2920b7f483df0fb60f623dab45900872a9f81ed6
James Gould <nobleburgundy@gmail.com>
2026-06-11 12:11:44 -0500

    Added download icon for music tracks

--- js/main.js
+++ js/main.js
@@ -148,9 +148,13 @@
           '<span class="track-num">' + String(i + 1).padStart(2, '0') + '</span>' +
           '<div class="track-info">' +
             '<span class="track-name">' + t.name + '</span>' +
-            '<span class="track-meta">' + (t.duration ? formatTime(t.duration) : '\u2026') + '</span>' +
-          '</div>';
-        div.addEventListener('click', function () {
+          '</div>' +
+          '<span class="track-meta">' + (t.duration ? formatTime(t.duration) : '\u2026') + '</span>' +
+          '<a class="track-download" href="' + t.src + '" download aria-label="Download ' + t.name + '">' +
+            '<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>' +
+          '</a>';
+        div.addEventListener('click', function (e) {
+          if (e.target.closest('.track-download')) return;
           loadTrack(i, true);
         });
         tracksList.appendChild(div);
